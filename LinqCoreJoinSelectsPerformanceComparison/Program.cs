e06a0531f0b10bf00e819f87ca0c951f35f5c468
Gregory Levit <grigoriy.levit@arcadia.spb.ru>
2018-12-24 20:14:00 +0300

    Add project files.

---
+++ LinqCoreJoinSelectsPerformanceComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqCoreJoinSelectsPerformanceComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
